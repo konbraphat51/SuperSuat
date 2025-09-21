b108fa5df1758ee020dd2f5e72626b15d5549b15
konbraphat <konbraphat@gmail.com>
2025-09-22 03:36:57 +0900

    paging

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -1,5 +1,14 @@
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+import { PageLogin } from "./pages/login/pageLogin.tsx";
+
 function App() {
-	return <></>;
+	return (
+		<BrowserRouter>
+			<Switch>
+				<Route path="/login" component={PageLogin} />
+			</Switch>
+		</BrowserRouter>
+	);
 }
 
 export default App;
---
+++ Front/src/pages/login/PageLogin.tsx
@@ -0,0 +1,7 @@
+export const PageLogin = () => {
+	return (
+		<div>
+			<h1>Login</h1>
+		</div>
+	);
+};
