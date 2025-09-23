6b6dd1538000830468c3b0a840ca51f46510dffa
konbraphat <konbraphat@gmail.com>
2025-09-24 06:50:31 +0900

    login / logout buttons

--- Front/src/components/Header.tsx
+++ Front/src/components/Header.tsx
@@ -1,7 +1,21 @@
+import { FetchSession } from "../scripts/SessionControl";
+
 export const Header = () => {
+	const session = FetchSession();
+
 	return (
 		<header>
 			<h1>Welcome to SuperSuat</h1>
+			{session.isLoggedIn ? (
+				<>
+					<p>You are logged in as {session.userName}</p>
+					<button onClick={() => (window.location.href = "/.auth/logout")}>Logout</button>
+				</>
+			) : (
+				<button onClick={() => (window.location.href = "/.auth/login/github")}>
+					Login with GitHub
+				</button>
+			)}
 		</header>
 	);
 };
