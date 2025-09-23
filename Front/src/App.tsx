02dab815e0375729caf7f8290a891cb59d183028
konbraphat <konbraphat@gmail.com>
2025-09-24 00:26:43 +0900

    route to login page

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { PageHome } from "./pages/home/PageHome";
+import { PageLogin } from "./pages/login/PageLogin";
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<PageHome />} />
+				<Route path="/login" element={<PageLogin />} />
 			</Routes>
 		</BrowserRouter>
 	);
