d91bec021dbcd1642bfcf1d83db28253b2ec6848
konbraphat <konbraphat@gmail.com>
2025-09-24 06:45:45 +0900

    base header

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -1,15 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Header } from "./components/Header";
 import { PageHome } from "./components/pages/home/PageHome";
 import { PageLogin } from "./components/pages/login/PageLogin";
 
 function App() {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<PageHome />} />
-				<Route path="/login" element={<PageLogin />} />
-			</Routes>
-		</BrowserRouter>
+		<>
+			<Header />
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<PageHome />} />
+					<Route path="/login" element={<PageLogin />} />
+				</Routes>
+			</BrowserRouter>
+		</>
 	);
 }
 
