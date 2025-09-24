e1052bf9f52f5e325317fad3be1f337ddbe78e7e
konbraphat <konbraphat@gmail.com>
2025-09-24 13:24:16 +0900

    create ordering page

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -3,6 +3,7 @@ import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { PageHome } from "./components/pages/home/PageHome";
 import { PageLogin } from "./components/pages/login/PageLogin";
+import { PagePdfOrdering } from "./components/pages/pdfOrdering/PagePdfOrdering";
 
 function App() {
 	return (
@@ -12,6 +13,7 @@ function App() {
 				<Routes>
 					<Route path="/" element={<PageHome />} />
 					<Route path="/login" element={<PageLogin />} />
+					<Route path="/ordering" element={<PagePdfOrdering />} />
 				</Routes>
 			</BrowserRouter>
 			<Footer />
---
+++ Front/src/components/pages/pdfOrdering/PagePdfOrdering.tsx
@@ -0,0 +1,7 @@
+export const PagePdfOrdering = () => {
+	return (
+		<div>
+			<h1>PDF Ordering</h1>
+		</div>
+	);
+};
