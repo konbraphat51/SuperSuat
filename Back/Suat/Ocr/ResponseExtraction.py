07ac61adbd158cf9aaa0703bcadb0874822c1538
konbraphat <konbraphat@gmail.com>
2025-09-29 08:52:40 +0900

    locally extract OCR data

--- Back/Suat/Ocr/ResponseExtraction.py
+++ Back/Suat/Ocr/ResponseExtraction.py
@@ -1,3 +1,7 @@
+from encodings.idna import sace_prefix
+from random import sample
+
+
 def extract_response_data(ocr_response: dict) -> dict:
     """
     Extracts relevant data from the OCR response.
@@ -24,3 +28,11 @@ def extract_response_data(ocr_response: dict) -> dict:
     result["figures"] = analyze_result["figures"]
 
     return result
+
+if __name__ == "__main__":
+    import json
+    with open("./sample_ocr.json", "r", encoding="utf-8") as f:
+        sample_data = json.load(f)
+    extracted_data = extract_response_data(sample_data)
+    with open("./extracted_data.json", "w", encoding="utf-8") as f:
+        f.write(json.dumps(extracted_data, indent=2))
