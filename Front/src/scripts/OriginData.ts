5fe22f40488664f68283646a69d991efaf308b81
konbraphat <konbraphat@gmail.com>
2025-10-03 12:00:02 +0900

    feat: add OriginData module with XML parsing and building functionality

---
+++ Front/src/scripts/OriginData.ts
@@ -0,0 +1,43 @@
+import { XMLParser, XMLBuilder } from "fast-xml-parser";
+
+export interface OriginData {
+	id: string;
+	items: OriginItem[];
+}
+
+export interface OriginItem {
+	index: number;
+	orderIndex: number;
+	type: "paragraph" | "figure";
+}
+
+export interface OriginParagraph extends OriginItem {
+	content: string;
+}
+
+export interface OriginFigure extends OriginItem {
+	imageId: string;
+	caption: string;
+}
+
+export const origin2xml = (data: OriginData): string => {
+	const builder = new XMLBuilder({
+		ignoreAttributes: false,
+	});
+	const xmlObject = {
+		"?xml": {
+			"@_version": "1.0",
+			"@_encoding": "UTF-8",
+		},
+		data: data,
+	};
+	return builder.build(xmlObject);
+};
+
+export const xml2origin = (xml: string): OriginData => {
+	const parser = new XMLParser({
+		ignoreAttributes: false,
+	});
+	const jsonObj = parser.parse(xml);
+	return jsonObj.data;
+};
