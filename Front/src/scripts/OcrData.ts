0675e0804cf8d6006742d5e5c2ff44a25cbd390b
konbraphat <konbraphat@gmail.com>
2025-10-06 18:57:00 +0900

    reorganize OcrData

--- Front/src/scripts/OcrData.ts
+++ Front/src/scripts/OcrData.ts
@@ -1,38 +1,26 @@
-export interface OcrData {
-	pages: OcrPage[];
-	paragraphs: OcrParagraph[];
-	figures: OcrFigure[];
-}
-
-export interface OcrPage {
-	width: number;
-	height: number;
-}
-
 export interface OcrParagraph {
-	spans: {
-		offset: number;
-		length: number;
-	}[];
-	boundingRegions: {
-		pageNumber: number;
-		polygon: [number, number];
-	};
+	polygon: [number, number];
 	role: string;
 	content: string;
 }
 
 export interface OcrFigure {
 	id: string;
-	boundingRegions: {
-		pageNumber: number;
-		polygon: [number, number];
-	}[];
+	polygon: [number, number];
 	caption: {
 		content: string;
-		boundingRegions: {
-			pageNumber: number;
-			polygon: [number, number];
-		}[];
+		polygon: [number, number];
 	} | null;
+}
+
+export interface OcrPage {
+	width: number;
+	height: number;
+	number: number;
+	paragraphs: OcrParagraph[];
+	figures: OcrFigure[];
+}
+
+export interface OcrData {
+	pages: OcrPage[];
 }
