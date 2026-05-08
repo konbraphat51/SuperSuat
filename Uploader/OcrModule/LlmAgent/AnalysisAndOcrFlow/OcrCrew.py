72c7974e4bdf4483d9bdd8bde7ada7bc7fce4f7f
konbraphat <konbraphat@gmail.com>
2026-05-08 14:33:36 +0900

    ocr agent

--- Uploader/OcrModule/LlmAgent/AnalysisAndOcrFlow/OcrCrew.py
+++ Uploader/OcrModule/LlmAgent/AnalysisAndOcrFlow/OcrCrew.py
@@ -1,8 +1,28 @@
-from crewai import Task
-from crewai.project import CrewBase
+from crewai import Agent, Task, agent
+from crewai.project import CrewBase, agent
 from crewai.agents.agent_builder.base_agent import BaseAgent
 
 @CrewBase
 class OcrCrew:
     agents: list[BaseAgent]
     tasks: list[Task]
+
+    @agent
+    def ocr_agent(self) -> Agent:
+        return Agent(
+            role = "OCR Agent for single page",
+            backstory = (
+                "This entire project aims to conduct OCR on the PDF document and convert it into Markdown file."
+                "This agent is responsible for conducting OCR for a single page of the PDF document, based on the layout analysis document created by the analyst agent."
+                "Before conducting OCR, an analysis agent conducted a layout analysis to unify the heading levels",
+                "This agent will receive the layout analysis document and the page to be OCRed."
+                "This agent is responsible for conducting OCR for the page based on the layout analysis document, and output the OCR result in a structured format."
+            ),
+            goal = (
+                "Conduct OCR for all information written in the given PDF page."
+            ),
+            llm = "bedrock/qwen.qwen3-vl-235b-a22b",
+            allow_delegation = True,
+            max_iter=30,
+            multimodal = True,
+        )
