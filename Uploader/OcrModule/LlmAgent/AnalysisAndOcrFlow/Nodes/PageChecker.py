ebf23faedeb8529986ba54ab3a520b289a09482d
konbraphat <konbraphat@gmail.com>
2026-05-11 05:26:00 +0900

    define output schema

--- Uploader/OcrModule/LlmAgent/AnalysisAndOcrFlow/Nodes/PageChecker.py
+++ Uploader/OcrModule/LlmAgent/AnalysisAndOcrFlow/Nodes/PageChecker.py
@@ -1,3 +1,6 @@
+from pydantic import BaseModel, Field
+
+
 SYSTEM_PROMPT = (
     "This entire project is to conduct OCR on scanned PDF documents. "
     "\nAn agent that have seen some pages of the document has analyzed the layout of the document,"
@@ -12,3 +15,16 @@ SYSTEM_PROMPT = (
     "You do not have to be too strict, but try not to miss any possible OCR inconsistency."
 )
 
+class OutputSchema(BaseModel):
+    page_passed: bool = Field(
+        description=(
+            "Whether the page layout is covered in the layout document and can pass to OCR transcription."
+        )
+    )
+
+    unknown_layout_styles: str | None = Field(
+        description=(
+            "If there is unknown layout style that may cause inconsistency in inter-page OCR results, "
+            "specify and describe the unknown layout style so that the layout analyst agent can update the layout."
+        )
+    )
