1e93e10c57d75d9d0cf68e3de342e4dec574a648
konbraphat <konbraphat@gmail.com>
2026-05-15 16:59:33 +0900

    move schema

--- Uploader/OcrModule/Ocr.py
+++ Uploader/OcrModule/Ocr.py
@@ -1,46 +1,7 @@
 from abc import ABC, abstractmethod
-from dataclasses import dataclass
-from typing import Literal
 from PIL.Image import Image
 
-
-@dataclass
-class OcrResultBlock:
-    block_type: Literal[
-        "text", "image", "table", "equation", "footer", "section"
-    ]
-
-
-class OcrResultBlockText(OcrResultBlock):
-    text: str
-
-
-class OcrResultBlockImage(OcrResultBlock):
-    image_data: bytes
-    caption: str
-
-
-class OcrResultBlockTable(OcrResultBlock):
-    text: str  # in Markdown format
-    caption: str
-
-
-class OcrResultBlockEquation(OcrResultBlock):
-    text: str  # in LaTeX format
-    caption: str
-
-
-class OcrResultBlockFooter(OcrResultBlock):
-    text: str
-
-
-class OcrResultBlockSection(OcrResultBlock):
-    text: str
-    level: int
-
-
-class OcrResult:
-    blocks: list[OcrResultBlock]
+from OcrModule.OcrSchema import OcrResult
 
 
 class Ocr(ABC):
