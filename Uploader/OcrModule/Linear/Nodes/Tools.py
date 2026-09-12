66beecb4da12de3c73e62779422f7c4057d4ce49
konbraphat51 <konbraphat@gmail.com>
2026-09-12 10:53:20 +0900

    add text block

--- Uploader/OcrModule/Linear/Nodes/Tools.py
+++ Uploader/OcrModule/Linear/Nodes/Tools.py
@@ -1,7 +1,8 @@
 import base64
+from typing import Literal
 from io import BytesIO
 from PIL.Image import Image
-from ...OcrSchema import OcrResultSection, OcrResultBlock
+from ...OcrSchema import OcrResultBlockText, OcrResultSection, OcrResultBlock, TEXT_BLOCK_TYPES
 
 def pil_to_base64(img: Image, format: str = "PNG") -> str:
     buffered = BytesIO()
@@ -24,6 +25,45 @@ def find_block_by_index(
             continue
 
     raise KeyError(f"Block with index {index} not found")
+
+def get_max_block_index(section: OcrResultSection) -> int:
+    max_index = -1
+
+    for block in section.section_content:
+        if block.block_index > max_index:
+            max_index = block.block_index
+
+    for child_section in section.child_sections:
+        child_max_index = get_max_block_index(child_section)
+        if child_max_index > max_index:
+            max_index = child_max_index
+
+    return max_index
+
+def find_section_by_index(
+    index: int,
+    section: OcrResultSection,
+) -> OcrResultSection:
+    if section.section_index == index:
+        return section
+
+    for child_section in section.child_sections:
+        try:
+            return find_section_by_index(index, child_section)
+        except KeyError:
+            continue
+
+    raise KeyError(f"Section with index {index} not found")
+
+def get_max_section_index(section: OcrResultSection) -> int:
+    max_index = section.section_index
+
+    for child_section in section.child_sections:
+        child_max_index = get_max_section_index(child_section)
+        if child_max_index > max_index:
+            max_index = child_max_index
+
+    return max_index
 
 class LinearTools:
     def __init__(
@@ -69,3 +109,26 @@ class LinearTools:
 
         block.text = text
         return f"Block with index {block_index} has been updated successfully. The new text is: \n{text}"
+
+    def add_text_block(
+        self,
+        section_index: int,
+        block_type: TEXT_BLOCK_TYPES,
+        text: str,
+    ) -> str:
+        try:
+            section = find_section_by_index(section_index, self.ocr_entire_section)
+        except KeyError:
+            return f"ERROR: Section with index {section_index} not found"
+
+        new_block_index = get_max_block_index(self.ocr_entire_section) + 1
+        new_block = OcrResultBlockText(
+            block_type=block_type,
+            existing_pages=[],
+            block_index=new_block_index,
+            text=text,
+            text_type=block_type
+        )
+        section.section_content.append(new_block)
+
+        return f"New text block added to section {section_index} with block index {new_block_index}. The text is: \n{text}"
