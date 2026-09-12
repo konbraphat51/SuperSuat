fb39fa593fc655eba948979dd0f57eab460140dc
konbraphat51 <konbraphat@gmail.com>
2026-09-12 12:21:30 +0900

    initialize root section

--- Uploader/OcrModule/Linear/LinearOcr.py
+++ Uploader/OcrModule/Linear/LinearOcr.py
@@ -1,6 +1,7 @@
 from PIL.Image import Image
 from langchain_core.language_models import BaseChatModel
 from ..Ocr import Ocr
+from ..OcrSchema import OcrResultSection
 
 class LinearOcr(Ocr):
     def __init__(
@@ -8,10 +9,18 @@ class LinearOcr(Ocr):
         ocr_model: BaseChatModel,
     ) -> None:
         self.ocr_model = ocr_model
+        self.entire_section: OcrResultSection
 
     def ocr(
          self,
          all_page_images: list[Image],
      ) -> OcrResult:
          # Implement the OCR logic here
-         pass
+        self._initialize_entire_section()
+
+    def _initialize_entire_section(self) -> None:
+        self.entire_section = OcrResultSection(
+            section_content=[],
+            section_index=0,
+            existing_pages=[],
+        )
